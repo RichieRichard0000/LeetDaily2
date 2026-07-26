81f841d8d95e70b2abaf346dd8b961e21bb99db5
Devansh Baranwal <83960769+RichieRichard0000@users.noreply.github.com>
2026-07-26 16:28:03 +0530

    Time: 30 ms (94.17%) | Memory: 22.4 MB (44.01%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,52 +1,30 @@
 class Solution {
 public:
-    unordered_map<string,int> dp;
-
-    int LCS(string s1, string s2) {
-        int n = s1.size();
-        int m = s2.size();
-
-        int dp[n+1][m+1];
-        memset(dp,0,sizeof(dp));
-
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s1[i-1]==s2[j-1])
-                    dp[i][j]=1+dp[i-1][j-1];
-                else
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
-            }
-        }
-
-        return dp[n][m];
-    }
-
-    int f(string curr, vector<string>& words){
-
-        if(dp.count(curr))
-            return dp[curr];
-
-        int ans=1;
-
-        for(int i=0;i<words.size();i++){
-
-            if(words[i].size()-curr.size()==1 &&
-               LCS(words[i],curr)==curr.size()){
-
-                ans=max(ans,1+f(words[i],words));
-            }
-
-        }
-
-        return dp[curr]=ans;
-    }
-
     int longestStrChain(vector<string>& words) {
 
-        int ans=1;
+        sort(words.begin(), words.end(),
+             [](string &a, string &b){
+                return a.size() < b.size();
+             });
 
-        for(int i=0;i<words.size();i++)
-            ans=max(ans,f(words[i],words));
+        unordered_map<string,int> dp;
+
+        int ans = 1;
+
+        for(string word : words){
+
+            dp[word] = 1;
+
+            for(int i=0;i<word.size();i++){
+
+                string prev = word.substr(0,i) + word.substr(i+1);
+
+                if(dp.count(prev))
+                    dp[word] = max(dp[word], dp[prev] + 1);
+            }
+
+            ans = max(ans, dp[word]);
+        }
 
         return ans;
     }
