5f749a81e904b1d0a1385b8157924f1f70e127ed
Devansh Baranwal <83960769+RichieRichard0000@users.noreply.github.com>
2026-09-02 12:44:32 +0530

    Time: 1 ms (42.00%) | Memory: 10.5 MB (89.02%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void rec(vector<int>& nums, vector<bool>& used, vector<int>& curr, vector<vector<int>>& ans) {
+        if (curr.size() == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (used[i]) continue;    
+            used[i] = true;
+            curr.push_back(nums[i]);
+            rec(nums, used, curr, ans);
+            curr.pop_back();          
+            used[i] = false;
+        }
+    }
+    
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        vector<bool> used(nums.size(), false);
+        rec(nums, used, curr, ans);
+        return ans;
+    }
+};
